2b1e042a812640f550da3ca1294998f2530f7c24
Rojit Shah <royce.rojit4@gmail.com>
2016-07-14 17:28:08 +0545

    DomainCreated

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Roj.IT</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="images/royce.jpg" type="image/x-icon">
+  <script src="https://use.fontawesome.com/f7c7d5c73a.js"></script>
+  <script type="text/javascript" src="js/jquery-2.1.4.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  
+<div class="wrapper">
+  <h3>Hi there! I am</h3>
+  <h1>ROJIT SHAH</h1>
+  <div class='content'>
+  <div class='visible'>
+    <p>
+      I&nbsp;
+    </p>
+    <ul>
+      <li>ntelligent</li>
+      <li>nductive</li>
+      <li>mmersive</li>
+      <li>nformative</li>
+      <li>nnate</li>
+      <li>ndomitable</li>
+      <li>ncredible</li>
+      <li>ntuitive</li>
+    </ul>
+  </div>
+
+</div>
+  <div class="social">
+    <a href="https://www.facebook.com/royce.rojit" target="_blank"><i class="fa fa-facebook"></i></a>
+    <a href="http://codepen.io" target="_blank"><i class="fa fa-codepen"></i></a>
+    <a href="https://github.com" target="_blank"><i class="fa fa-github"></i></a>
+    <a href=""><i class="fa fa-envelope"></i></a>
+  </div>
+</div>
+  <div class="mario_btn">
+    <a href="mario/index.html" target="_blank">
+      <img src="images/mario.png">
+    </a>
+  </div>
+
+
+</body>
+</html>
